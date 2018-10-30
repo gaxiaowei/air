216349af45fed8156178f8c2c7fb9e16503316e7
John <53950451@qq.com>
2018-10-30 19:41:33 +0800

    database mongo

--- src/Database/Query.php
+++ src/Database/Query.php
@@ -3,18 +3,18 @@ namespace Air\Database;
 
 interface Query
 {
-	public function insert(array $data);
-	public function update(array $data);
-	public function delete($data);
-	public function select(string $columns = '*');
-	public function where($condition, $bind);
-	public function group(string $fields);
-	public function having(string $condition, $bind);
-	public function order(string $field, string $direction = 'ASC');
-	public function orderAsc(string $field);
-	public function orderDesc(string $field);
-	public function limit(int $limit);
-	public function offset(int $offset);
+	public function insert(array $data) : Query;
+	public function update(array $data) : Query;
+	public function delete($data) : Query;
+	public function select(string $columns = '*') : Query;
+	public function where($condition, $bind) : Query;
+	public function group(string $fields) : Query;
+	public function having(string $condition, $bind) : Query;
+	public function order(string $field, string $direction = 'ASC') : Query;
+	public function orderAsc(string $field) : Query;
+	public function orderDesc(string $field) : Query;
+	public function limit(int $limit) : Query;
+	public function offset(int $offset) : Query;
 	public function get(array $column = ['*']);
 	public function find($id, array $column = ['*']);
 	public function first(array $column = ['*']);
