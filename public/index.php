4827989bdf94f5707526810b5c84560fb65d0824
John <53950451@qq.com>
2018-12-03 17:58:10 +0800

    index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
+$composerLoader = require __DIR__.'/../vendor/autoload.php';
 
 $air = new Air\Air(realpath(__DIR__.'/../'));
+
+$composerLoader->addPsr4('App\\', $air->getAppDirPath());
+unset($composerLoader);
 
 $air->server('ng')->run();
